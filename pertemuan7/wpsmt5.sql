2f016d04f40156cd0a359e491eff87012e044d45
Nurulihza25 <11ihzanoerol@gamil.com>
2022-01-22 18:44:22 +0700

    penyerahan tugas

---
+++ pertemuan7/wpsmt5.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Jan 2022 pada 09.37
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wpsmt5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_mhs`
+--
+
+CREATE TABLE `calon_mhs` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(9) NOT NULL,
+  `agama` varchar(9) NOT NULL,
+  `sekolah_asal` varchar(20) NOT NULL,
+  `foto_maba` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `calon_mhs`
+--
+
+INSERT INTO `calon_mhs` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`, `foto_maba`) VALUES
+(1, 'nurulihza', 'Batuceper', 'laki laki', 'islam', 'Ma attaqwa', 'foto1.jpg'),
+(2, 'muhammad fikri', 'kampung baru', 'laki laki', 'islam', 'Sma 14', 'foto1.jpg'),
+(3, 'muhammad iqbal', 'teluknaga', 'laki laki', 'islam', 'Sma 4', 'foto1.jpg'),
+(4, 'ilham romadhona', 'cipondoh', 'laki laki', 'islam', 'Sma 6', 'foto1.jpg'),
+(5, 'faisal setioardi', 'cimone', 'laki laki', 'islam', 'Sma 1', 'foto1.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
